2da5caa6a8f8654502fa4e54124f621794612be0
Sergey Manshin <vvintpann@gmail.com>
2022-07-19 11:45:23 +0300

    Export types

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,3 +2,14 @@ import './styles.scss';
 
 export { StoryBox } from './components/story-box';
 export * from './controls/hooks';
+export type {
+    StringControl,
+    Control,
+    RadioControl,
+    BaseControl,
+    BooleanControl,
+    ButtonControl,
+    NumberControl,
+    CheckboxControl,
+    UseControl,
+} from './type';

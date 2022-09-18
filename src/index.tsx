151c7cfdccf32100f9b5c12f6e6bd3b040a23d6f
Sergey Manshin <vvintpann@gmail.com>
2022-09-18 15:19:44 +0300

    Update react to 18.2.0

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { DemoApp } from './dev/app';
 
-ReactDOM.render(<DemoApp />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(<DemoApp />);
